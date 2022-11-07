805242a31b5c1d21dc29779820bfcb31c8c6dc2f
DylanKoehler <dylankoehler3212@gmail.com>
2022-11-07 09:43:03 -0600

    Created First 2 Dice that can roll with each being its own number.

--- Dice Game/ContentView.swift
+++ Dice Game/ContentView.swift
@@ -1,21 +1,59 @@
 //
 //  ContentView.swift
-//  Dice Game
+//  Roll the Dice
 //
-//  Created by Dylan Koehler on 11/7/22.
+//  Created by Dylan Koehler on 11/07/22
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomValue1 = 0
+    @State private var randomValue2 = 0
+    @State private var rotation = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Dice Roll")
+                .font(.title)
+                .padding()
+            Image("pips \(randomValue1)")
+                .resizable()
+                .frame(width: 200, height: 200, alignment: .center)
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                .padding()
+                .onTapGesture {
+                    chooseRandom(times: 3)
+                    withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                        rotation += 360
+                    }
+                }
+            Spacer()
         }
-        .padding()
+        VStack {
+            Image("pips \(randomValue2)")
+                .resizable()
+                .frame(width: 200, height: 200, alignment: .center)
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                .padding()
+                .onTapGesture {
+                    chooseRandom(times: 3)
+                    withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                        rotation += 360
+                    }
+                }
+            Spacer()
+        }
+    }
+    func chooseRandom(times:Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue1 = Int.random(in: 1...6)
+                randomValue2 = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+            }
+        }
     }
 }
 
@@ -24,3 +62,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+

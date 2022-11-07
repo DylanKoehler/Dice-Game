9be269f2d2076db936b2cd839646da0193e8d688
DylanKoehler <dylankoehler3212@gmail.com>
2022-11-07 09:40:16 -0600

    Initial Commit

---
+++ Dice Game/Dice_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_GameApp.swift
+//  Dice Game
+//
+//  Created by Dylan Koehler on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Dice_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
